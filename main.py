a4b1c1d8a0aabe5fd2dae6aadebb58fd4e8fb8ab
EDiasAlberto <proxamon1@gmail.com>
2021-06-02 17:50:04 +0100

    changed to flask app, added login page; need to fix authentication

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from flask import Flask as fl
+from flask import render_template
 import os
 
 webapp = fl(__name__)
 
 @webapp.route("/")
-def launchPage():
-    return "127.0.0.1"
+def index():
+    return render_template("login.html")
+
+@webapp.route("/home")
+def home():
+    return render_template("index.html")
+
+@webapp.route("/listings")
+def listings():
+    return render_template("listings.html")
+
+@webapp.route("/account")
+def account():
+    return render_template("account.html")
 
 @webapp.route("/secret/")
 def secretPage():
